8b7594f87b6e25df5267fc68cd8d7d3837f9bea6
Stevan Little <stevan.little@gmail.com>
2016-06-24 18:50:46 -0400

    add a command base class

---
+++ lib/App/Critique/Command.pm
@@ -0,0 +1,32 @@
+package App::Critique::Command;
+
+use strict;
+use warnings;
+
+use Term::ReadKey ();
+
+use constant TERM_WIDTH => (Term::ReadKey::GetTerminalSize())[0];
+use constant HR_ERROR   => ('== ERROR ', ('=' x (TERM_WIDTH - 9)));
+use constant HR_DARK    => ('=' x TERM_WIDTH);
+use constant HR_LIGHT   => ('-' x TERM_WIDTH);
+
+use App::Cmd::Setup -command;
+
+sub output {
+    my ($self, $msg, @args) = @_;
+    print((sprintf $msg, @args), "\n");
+}
+
+sub runtime_error {
+    my ($self, $msg, @args) = @_;
+    die "\n", HR_ERROR, "\n", (sprintf $msg, @args), "\n", HR_DARK, "\n";
+}
+
+1;
+
+__END__
+
+=pod
+
+=cut
+
