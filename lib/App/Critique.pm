33fe60405eeab4e4db91cecddbc1680b780ec627
Stevan Little <stevan.little@gmail.com>
2016-07-23 01:11:43 +0200

    better defaults on the config

--- lib/App/Critique.pm
+++ lib/App/Critique.pm
@@ -6,26 +6,26 @@ use warnings;
 our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:STEVAN';
 
-our %CONFIG; 
+our %CONFIG;
 
 BEGIN {
-    $App::Critique::CONFIG{'COLOR'}   = $ENV{'CRITIQUE_COLOR'};
-    $App::Critique::CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'};
-    $App::Critique::CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'};
-    $App::Critique::CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'};
-    
+    $App::Critique::CONFIG{'COLOR'}   = $ENV{'CRITIQUE_COLOR'}   || 0;
+    $App::Critique::CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'}   || 0;
+    $App::Critique::CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'} || 0;
+    $App::Critique::CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'}  || $ENV{'EDITOR'} || $ENV{'VISUAL'};
+
     # okay, we give you sensible Perl & Git defaults
-    $App::Critique::CONFIG{'IGNORE'} = { 
-        '.git'   => 1, 
-        'blib'   => 1, 
-        'local'  => 1, 
+    $App::Critique::CONFIG{'IGNORE'} = {
+        '.git'   => 1,
+        'blib'   => 1,
+        'local'  => 1,
         '_build' => 1,
-    };   
-    
+    };
+
     # then we add in any of yours
     if ( my $ignore = $ENV{'CRITIQUE_IGNORE'} ) {
         $App::Critique::CONFIG{'IGNORE'}->{ $_ } = 1
-            foreach split /\:/ => $ignore;    
+            foreach split /\:/ => $ignore;
     }
 }
 
