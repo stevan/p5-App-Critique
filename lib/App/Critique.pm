2a867f16adb97ec76092e35fbb9d4b68ebf850f1
Stevan Little <stevan.little@booking.com>
2016-07-22 17:52:39 +0200

    move the config handling from the script to the App::Critique module, then add a test for it

--- lib/App/Critique.pm
+++ lib/App/Critique.pm
@@ -6,7 +6,14 @@ use warnings;
 our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:STEVAN';
 
-our %CONFIG;
+our %CONFIG; 
+
+BEGIN {
+    $App::Critique::CONFIG{'COLOR'}   = $ENV{'CRITIQUE_COLOR'};
+    $App::Critique::CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'};
+    $App::Critique::CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'};
+    $App::Critique::CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'};
+}
 
 use App::Cmd::Setup -app => {
     plugins => [
