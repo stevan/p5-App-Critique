9ac42c9b17c3dc7950b979d5d31969f4ad6c64e3
Stevan Little <stevan.little@booking.com>
2016-07-22 18:19:05 +0200

    keep sensible ones in place, let the rest be added

--- lib/App/Critique.pm
+++ lib/App/Critique.pm
@@ -13,6 +13,20 @@ BEGIN {
     $App::Critique::CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'};
     $App::Critique::CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'};
     $App::Critique::CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'};
+    
+    # okay, we give you sensible Perl & Git defaults
+    $App::Critique::CONFIG{'IGNORE'} = { 
+        '.git'   => 1, 
+        'blib'   => 1, 
+        'local'  => 1, 
+        '_build' => 1,
+    };   
+    
+    # then we add in any of yours
+    if ( my $ignore = $ENV{'CRITIQUE_IGNORE'} ) {
+        $App::Critique::CONFIG{'IGNORE'}->{ $_ } = 1
+            foreach split /\:/ => $ignore;    
+    }
 }
 
 use App::Cmd::Setup -app => {
