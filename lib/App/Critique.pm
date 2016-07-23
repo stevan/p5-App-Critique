3151e11d5f8565b5cc13f5b51f183bb6b48a65fe
Stevan Little <stevan.little@gmail.com>
2016-07-24 00:08:40 +0200

    no need to fully qualify something that is in the local scope

--- lib/App/Critique.pm
+++ lib/App/Critique.pm
@@ -11,14 +11,14 @@ our $AUTHORITY = 'cpan:STEVAN';
 our %CONFIG;
 
 BEGIN {
-    $App::Critique::CONFIG{'HOME'}    = $ENV{'CRITIQUE_HOME'}    || File::HomeDir->my_home;
-    $App::Critique::CONFIG{'COLOR'}   = $ENV{'CRITIQUE_COLOR'}   || 0;
-    $App::Critique::CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'}   || 0;
-    $App::Critique::CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'} || 0;
-    $App::Critique::CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'}  || $ENV{'EDITOR'} || $ENV{'VISUAL'};
+    $CONFIG{'HOME'}    = $ENV{'CRITIQUE_HOME'}    || File::HomeDir->my_home;
+    $CONFIG{'COLOR'}   = $ENV{'CRITIQUE_COLOR'}   || 0;
+    $CONFIG{'DEBUG'}   = $ENV{'CRITIQUE_DEBUG'}   || 0;
+    $CONFIG{'VERBOSE'} = $ENV{'CRITIQUE_VERBOSE'} || 0;
+    $CONFIG{'EDITOR'}  = $ENV{'CRITIQUE_EDITOR'}  || $ENV{'EDITOR'} || $ENV{'VISUAL'};
 
     # okay, we give you sensible Perl & Git defaults
-    $App::Critique::CONFIG{'IGNORE'} = {
+    $CONFIG{'IGNORE'} = {
         '.git'   => 1,
         'blib'   => 1,
         'local'  => 1,
@@ -27,7 +27,7 @@ BEGIN {
 
     # then we add in any of yours
     if ( my $ignore = $ENV{'CRITIQUE_IGNORE'} ) {
-        $App::Critique::CONFIG{'IGNORE'}->{ $_ } = 1
+        $CONFIG{'IGNORE'}->{ $_ } = 1
             foreach split /\:/ => $ignore;
     }
 }
