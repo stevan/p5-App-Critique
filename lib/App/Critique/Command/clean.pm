0b9cedde2f2256760af297edfc421739de533e19
Stevan Little <stevan.little@booking.com>
2016-10-28 11:24:43 +0200

    Quick and dirty file cleaner for when you update the working directory and do not want to re-run the collector

---
+++ lib/App/Critique/Command/clean.pm
@@ -0,0 +1,81 @@
+package App::Critique::Command::clean;
+
+use strict;
+use warnings;
+
+our $VERSION   = '0.05';
+our $AUTHORITY = 'cpan:STEVAN';
+
+use App::Critique::Session;
+
+use App::Critique -command;
+
+sub opt_spec {
+    my ($class) = @_;
+    return (
+        [ 'dry-run', 'display the pruned list of files, but do not overwrite' ],
+        [],
+        $class->SUPER::opt_spec,
+    );
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+
+    my $session = $self->cautiously_load_session( $opt, $args );
+
+    info('Session file loaded.');
+
+    my @removed_files;
+    my @preserved_files;
+    
+    my @tracked_files = $session->tracked_files;
+    
+    info('Reviewing %d file(s).', format_number(scalar @tracked_files));
+    
+    foreach my $file ( @tracked_files ) {
+        if ( -e $file->path ) {
+            push @preserved_files => $file;
+        }
+        else {
+            push @removed_files => $file;
+        }
+    }
+    
+    info('Found %d removed file(s).', format_number(scalar @removed_files));
+    
+    if ( $opt->verbose || $opt->dry_run ) {
+        info(HR_LIGHT);
+        info($_->path) foreach @removed_files;
+        info(HR_LIGHT);
+    }
+    
+    if ( $opt->dry_run ) {
+        info('[dry-run] Would have updated list of %s file(s).', format_number(scalar @preserved_files));
+    }
+    else {
+        $session->set_tracked_files( @preserved_files );
+        $session->reset_file_idx;
+        info('Sucessfully updated list of %s file(s).', format_number(scalar @preserved_files));
+
+        $self->cautiously_store_session( $session, $opt, $args );
+        info('Session file stored successfully (%s).', $session->session_file_path);
+    }
+
+}
+
+1;
+
+__END__
+
+# ABSTRACT: Clean up the set of file for the current critique session
+
+=pod
+
+=head1 DESCRIPTION
+
+This command will clean up the set of files for the current critique 
+session. If a file has been deleted in the filesystem, this will also 
+remove that file from the critique session as well.
+
+=cut
